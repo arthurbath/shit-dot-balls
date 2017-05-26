2bc24ac2106cf8f5631c454e7b2201b4778bb1e6
Arthur Bath <arthurbath@icloud.com>
2017-05-26 12:05:05 -0700

    Video player

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,43 @@
-$(document).ready(() => {
-	$('.scripts-fail').hide()
-	$('.scripts-success').show()
-})
+// Inject YouTube player script
+let tag = document.createElement('script')
+tag.src = 'https://www.youtube.com/iframe_api'
+let firstScriptTag = document.getElementsByTagName('script')[0]
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+
+let firstLoad = true
+let videoPlayer
+function onYouTubeIframeAPIReady () { // eslint-disable-line no-unused-vars
+	videoPlayer = new YT.Player('videoPortal', {
+		height: '300',
+		width: '400',
+		playerVars: {
+			color: 'white',
+			modestbranding: 1,
+			enablejsapi: 1,
+			showinfo: 0,
+			controls: 2,
+			iv_load_policy: 3,
+		},
+		events: {
+			onReady: event => {
+				event.target.cuePlaylist({
+					listType: 'playlist',
+					list: 'PLq3AXW8g_v5N1sZ5_LoNYGqjtSCrOs5YY',
+				})
+			},
+			onStateChange: event => {
+				// On playlist initially queued
+				if (firstLoad && event.data === YT.PlayerState.CUED) {
+					event.target.setVolume(100) // User should control volume from their device
+					event.target.setShuffle(true)
+					event.target.setLoop(true)
+					event.target.playVideoAt(0) // Play first video in shuffled list
+				}
+			},
+			onError: event => {
+				console.warn('ERROR PLAYING VIDEO', event)
+				event.target.nextVideo()
+			},
+		},
+	})
+}
